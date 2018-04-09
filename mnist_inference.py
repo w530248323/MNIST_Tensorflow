21e2d1d6bf2da2d125b3f0635689733aa3f5be8e
Wang Jingyao <530248323@qq.com>
2018-04-09 17:40:47 +0800

    Add files via upload

---
+++ mnist_inference.py
@@ -0,0 +1,100 @@
+# -*- coding: utf-8 -*-
+"""
+INPUT: [28x28x1]             weights: 0
+CONV5-32: [28x28x32]         weights: (5*5*1+1)*32
+POOL2: [14x14x32]            weights: 0
+CONV5-64: [14x14x64]         weights: (5*5*32+1)*64
+POOL2: [7x7x64]              weights: 0
+FC: [1x1x512]                weights: (7*7*64+1)*512
+FC: [1x1x10]                 weights: (1*1*512+1)*10
+"""
+import tensorflow as tf
+
+# 定义神经网络相关的参数
+INPUT_NODE = 784
+OUTPUT_NODE = 10
+
+IMAGE_SIZE = 28
+NUM_CHANNELS = 1
+NUM_LABELS = 10
+
+# 第一层卷积层的尺寸和深度
+CONV1_DEEP = 32
+CONV1_SIZE = 5
+# 第二层卷积层的尺寸和深度
+CONV2_DEEP = 64
+CONV2_SIZE = 5
+# 全连接层的节点个数
+FC_SIZE = 512
+
+
+# 定义神经网络的前向传播过程
+# 参数train用于区别训练过程和测试过程。训练过程引入dropout，可以进一步提升模型可靠性并防止过拟合
+def inference(input_tensor, train):
+    # 声明第一层神经网络的变量并完成前向传播过程
+    # 和标准LeNet-5模型不大一样，这里定义卷积层的输入为28*28*1的原始MNIST图片像素
+    # 因为卷积层中使用了全0填充，所以输出为28*28*32的矩阵
+    with tf.variable_scope('layer1-conv1'):  # variable_scope:大部分情况下，跟 tf.get_variable() 配合使用，实现变量共享的功能。
+        conv1_weights = tf.get_variable(
+            "weight", [CONV1_SIZE, CONV1_SIZE, NUM_CHANNELS, CONV1_DEEP],
+            initializer=tf.truncated_normal_initializer(stddev=0.1))  # 截断正态分布初始化参数
+        conv1_biases = tf.get_variable("bias", [CONV1_DEEP], initializer=tf.constant_initializer(0.0))  # 偏置初始化为0
+        # 使用边长为5，深度为32的过滤器，过滤器移动的步长为1，且使用全0填充
+        conv1 = tf.nn.conv2d(input_tensor, conv1_weights, strides=[1, 1, 1, 1],
+                             padding='SAME')  # [batch, height, width, channels]
+        relu1 = tf.nn.relu(tf.nn.bias_add(conv1, conv1_biases))
+
+    # 实现第二层池化层的前向传播过程。
+    # 这里选用最大池化层，池化层过滤器的边长为2，使用全0填充且移动的步长为2
+    # 这一层的输入是28*28*32的矩阵，输出为14*14*32的矩阵
+    with tf.name_scope('layer2-pool'):  # name_scope: 为了更好地管理变量的命名空间而提出的
+        pool1 = tf.nn.max_pool(relu1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+
+    # 声明第三层卷积层的变量并实现前向传播过程
+    # 这一层的输入为14*14*32的矩阵，输出为14*14*64的矩阵
+    with tf.variable_scope('layer3-conv2'):
+        conv2_weights = tf.get_variable(
+            "weight", [CONV2_SIZE, CONV2_SIZE, CONV1_DEEP, CONV2_DEEP],
+            initializer=tf.truncated_normal_initializer(stddev=0.1)
+        )
+        conv2_biases = tf.get_variable("bias", [CONV2_DEEP], initializer=tf.constant_initializer(0.0))
+        # 使用边长为5，深度为64的过滤器，过滤器移动的步长为1，且使用全0填充
+        conv2 = tf.nn.conv2d(pool1, conv2_weights, strides=[1, 1, 1, 1], padding='SAME')
+        relu2 = tf.nn.relu(tf.nn.bias_add(conv2, conv2_biases))
+
+    # 实现第四层池化层的前向传播过程
+    # 这一层和第二层的结构是一样的。这一层的输入为14*14*64的矩阵，输出为7*7*64的矩阵。
+    with tf.name_scope('layer4-poo2'):
+        pool2 = tf.nn.max_pool(relu2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+
+    # 将第四层池化层的输出转化为第五层全连接层的输入格式
+    # 第四层的输出为7*7*64的矩阵，然而第五层全连接层需要的输入格式为向量，所以在这里需要将这个7*7*64的矩阵拉直成一个向量
+    pool_shape = pool2.get_shape().as_list()  # pool2.get_shape函数可以得到第四层输出矩阵的维度而不需要手工计算
+    # 注意因为每一层神经网络的输入输出都为一个batch的矩阵，所以这里得到的维度也包含了一个batch中数据的个数
+    nodes = pool_shape[1] * pool_shape[2] * pool_shape[3]  # 计算将矩阵拉直成向量之后的长度，这个长度就是矩阵长度及深度的乘积
+    # 通过tf.reshape函数将第四层的输出变成一个batch的向量
+    # pool_shape[0]为一个batch中样本的个数
+    reshaped = tf.reshape(pool2, [pool_shape[0], nodes])
+
+    # 声明第五层全连接层的变量并实现前向传播过程
+    # 这一层的输入是拉直之后的一组向量，向量长度为7*7*64=3136，输出是一组长度为512的向量
+    # dropout在训练时会随机将部分节点的输出改为0，可以避免过拟合问题，dropout一般只在全连接层而不是卷积层或者池化层使用
+    with tf.variable_scope('layer5-fc1'):
+        fc1_weights = tf.get_variable("weight", [nodes, FC_SIZE],
+                                      initializer=tf.truncated_normal_initializer(stddev=0.1))
+        fc1_biases = tf.get_variable('bias', [FC_SIZE], initializer=tf.constant_initializer(0.1))
+        fc1 = tf.nn.relu(tf.matmul(reshaped, fc1_weights) + fc1_biases)
+        if train:
+            fc1 = tf.nn.dropout(fc1, 0.5)
+
+    # 声明第六层全连接层的变量并实现前向传播过程。
+    # 这一层的输入是一组长度为512的向量，输出是一组长度为10的向量。
+    # 这一层的输出通过Softmax之后就得到了最后的分类结果。
+    with tf.variable_scope('layer6-fc2'):
+        fc2_weights = tf.get_variable("weight", [FC_SIZE, NUM_LABELS],
+                                      initializer=tf.truncated_normal_initializer(stddev=0.1))
+        fc2_biases = tf.get_variable('bias', [NUM_LABELS], initializer=tf.constant_initializer(0.1))
+        logit = tf.matmul(fc1, fc2_weights) + fc2_biases
+
+    # 返回第六层的输出
+    return logit
---
+++ mnist_train.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+import os
+import numpy as np
+
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+import mnist_inference
+
+
+# 配置神经网络的参数
+BATCH_SIZE = 100
+LEARNING_RATE_BASE = 0.01
+LEARNING_RATE_DECAY = 0.99
+TRAINING_STEPS = 30000
+
+# 模型保存的路径和文件名
+MODEL_SAVE_PATH = "model/"
+MODEL_NAME = "model.ckpt"
+
+
+def train(mnist):
+    # 定义输入输出placeholder（占位符），输入为一个四维矩阵
+    x = tf.placeholder(tf.float32, [
+        BATCH_SIZE,  # 第一维表示一个batch中样例的个数
+        mnist_inference.IMAGE_SIZE,  # 第二维和第三维表示图片的尺寸
+        mnist_inference.IMAGE_SIZE,
+        mnist_inference.NUM_CHANNELS],  # 第四维表示图片的深度
+                       name='x-input')
+    y_ = tf.placeholder(tf.float32, [None, mnist_inference.OUTPUT_NODE], name='y-input')
+
+    # 直接使用mnist_inference.py中定义的前向传播过程
+    y = mnist_inference.inference(x, True)
+    global_step = tf.Variable(0, trainable=False)   # 迭代的计数器
+
+    # 定义损失函数、学习率以及训练过程
+    # 计算交叉熵作为刻画预测值和真实值之间差距的损失函数，这里使用Tensorflow中提供的
+    # sparse_softmax_cross_entropy_with_logits函数来计算交叉熵。当分类问题只有一个正确答案时，
+    # 可以使用这个函数来加速交叉熵的计算。这个函数的第一个参数是神经网络不包括softmax的前向传播结果
+    # 第二个是训练数据的正确答案。因为标准答案是一个长度为10的一维数组，而该函数需要提供的是
+    # 一个正确答案的数字，所以需要使用tf.argmax(y_,1)函数来得到正确答案的编号
+    cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=y, labels=tf.argmax(y_, 1))
+    cross_entropy_mean = tf.reduce_mean(cross_entropy)  # 计算在当前batch中所有样例的交叉熵平均值。
+    loss = cross_entropy_mean
+    learning_rate = tf.train.exponential_decay(LEARNING_RATE_BASE, global_step, mnist.train.num_examples / BATCH_SIZE,
+                                               LEARNING_RATE_DECAY, staircase=True)     # 学习率下降，每训练550轮，学习率乘以0.99
+    train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss, global_step=global_step)   # 小批量梯度下降
+
+    # 在训练神经网络模型时，每过一遍数据要通过反向传播来更新神经网络中的参数。
+    # TensorFlow提供了tf.control_dependencies和tf.group两种机制。
+    # 下面两行程序和train_op = tf.group(train_step, variables_averages_op)是等价的。
+    # with tf.control_dependencies([train_step]):
+    #    train_op = tf.no_op(name='train')    # Does nothing. Only useful as a placeholder for control edges.
+    # tf.control_dependencies()控制计算流图的，给图中的某些计算指定顺序，可以确保train_step在被刷新之后,再执行定义的内容，从而保证计算顺序的正确性。
+    train_op = tf.group(train_step)
+
+    # 初始化Tensorflow持久化类
+    saver = tf.train.Saver()
+    with tf.Session() as sess:  # 创建一个会话
+        tf.global_variables_initializer().run()     # 初始化模型的参数
+        for i in range(TRAINING_STEPS):
+            xs, ys = mnist.train.next_batch(BATCH_SIZE)     # 通过next_batch()就可以一个一个batch的拿数据
+            # 类似地将输入的训练数据格式调整为一个四维矩阵，并将这个调整后的数据传入sess.run过程
+            reshaped_xs = np.reshape(xs, (BATCH_SIZE,
+                                          mnist_inference.IMAGE_SIZE,
+                                          mnist_inference.IMAGE_SIZE,
+                                          mnist_inference.NUM_CHANNELS))
+            # 占位符并没有初始值，它只会分配必要的内存。在会话中，占位符可以使用feed_dict馈送数据。
+            _, loss_value, step = sess.run([train_op, loss, global_step], feed_dict={x: reshaped_xs, y_: ys})       # ？？？？？？？？？
+            # 每1000轮保存一次模型
+            if i % 100 == 0:
+                # 输出当前的训练情况。这里只输出了模型在当前训练batch上的损失函数大小。通过损失函数的大小可以大概了解训练的情况
+                # 在验证数据集上的正确率信息会有一个单独的程序来生成
+                print("After %d training step(s), loss on training batch is %f." % (step, loss_value))
+                # 保存当前的模型。注意这里隔出了global_step参数，这样可以让每个被保存模型的文件名末尾加上训练的轮数
+                # 比如“model.ckpt-1000”表示训练1000轮后得到的模型
+                saver.save(sess, os.path.join(MODEL_SAVE_PATH, MODEL_NAME), global_step=global_step)
+
+
+def main(argv=None):
+    mnist = input_data.read_data_sets("dataset/", one_hot=True)
+    train(mnist)
+
+
+if __name__ == '__main__':
+    tf.app.run()
